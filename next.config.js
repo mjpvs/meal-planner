f00d06e6771a59043f4ed30b927a3c57e2afd186
mprosser <Michael.Prosser@visitscotland.com>
2026-03-19 14:32:30 +0000

    Optimise for smaller build

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+    experimental: {
+        optimizePackageImports: ['lucide-react'],
+    },
+    swcMinify: false,
 };
 
 module.exports = nextConfig;
